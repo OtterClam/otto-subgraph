e7bb745cf6be2ee4e497407abee50709d5345643
0xthk <kf013099@gmail.com>
2022-04-03 18:50:15 +0800

    feat: update contract address

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,3 +1,3 @@
 //Tokens definition
-export const OTTO = '0xF0a0328bc4C1FB00D4fa100BC942fd2F44F25d95'
+export const OTTO = '0xB35617516850310200a1f8A96858e3306f5EB9c3'
 export const OTTO_V2_BLOCK = '25765581'
