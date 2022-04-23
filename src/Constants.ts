2cfb8ceee5c80e9345228c58f856325ca34d56db
0xthk <kf013099@gmail.com>
2022-04-23 18:49:56 +0800

    chore: set mainnet address

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,6 +1,6 @@
 //Tokens definition
-export const OTTO = '0xd2BBb888580dEdDDE12Aa9Ca6b7C2DacA59eFC42'
-export const OTTO_V2_BLOCK = '26049922'
-export const OTTO_V3_BLOCK = '26049922'
+export const OTTO = '0x6e8A9Cb6B1E73e9fCe3FD3c68b5af9728F708eB7'
+export const OTTO_V2_BLOCK = '26538570'
+export const OTTO_V3_BLOCK = '27477979'
 
-export const OTTO_ITEM = '0xb884E2FF1bf547AAC4DF0671c519ee029F1F5A50'
+export const OTTO_ITEM = '0x27DFfd9016829885712810671db56941B97f6C46'
